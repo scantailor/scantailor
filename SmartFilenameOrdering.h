25f3bfaf564330e9288f080fb272871da6b7d220
jart <jart@80b70061-164d-0410-b7b7-e274ec932871>
2008-06-19 14:35:12 +0000

    Document the SmartFilenameOrdering class.
    git-svn-id: https://scantailor.svn.sourceforge.net/svnroot/scantailor/trunk@40 80b70061-164d-0410-b7b7-e274ec932871

--- SmartFilenameOrdering.h
+++ SmartFilenameOrdering.h
@@ -26,6 +26,16 @@ class SmartFilenameOrdering
 public:
 	SmartFilenameOrdering() {}
 	
+	/**
+	 * \brief Compare filenames using a set of heuristic rules.
+	 *
+	 * This function tries to mimic the way humans would order filenames.
+	 * For example, "2.png" will go before "12.png".  While doing so,
+	 * it still provides the usual guarantees of a comparison predicate,
+	 * such as two different file paths will never be ruled equivalent.
+	 *
+	 * \return true if \p lhs should go before \p rhs.
+	 */
 	bool operator()(QFileInfo const& lhs, QFileInfo const& rhs) const;
 };
 
