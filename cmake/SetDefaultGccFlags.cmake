2e9154438bc0eaf4cb680c9a816e82b9faedee6f
jart <jart@80b70061-164d-0410-b7b7-e274ec932871>
2008-09-29 22:11:37 +0000

    Let user alter default compiler flags.
    git-svn-id: https://scantailor.svn.sourceforge.net/svnroot/scantailor/trunk@139 80b70061-164d-0410-b7b7-e274ec932871

--- cmake/SetDefaultGccFlags.cmake
+++ cmake/SetDefaultGccFlags.cmake
@@ -23,80 +23,90 @@ MACRO(ST_SET_DEFAULT_GCC_FLAGS)
 			SET(visibility_cflags_ "")
 		ENDIF(visibility_supported_)
 		
-		# Flags common for all build configurations
-		SET(
-			CMAKE_CXX_FLAGS
-			"-Wall -Wno-unused -ffast-math"
-			CACHE STRING "Common C++ flags for all build configurations." FORCE
-		)
-		SET(
-			CMAKE_EXE_LINKER_FLAGS "" CACHE STRING
-			"Common link flags for all build configurations." FORCE
-		)
+		IF(NOT COMPILER_FLAGS_OVERRIDDEN)
+			# Flags common for all build configurations.
+			SET(
+				CMAKE_C_FLAGS
+				"-Wall -Wno-unused -ffast-math"
+				CACHE STRING "Common C flags for all build configurations." FORCE
+			)
+			SET(
+				CMAKE_C_FLAGS "${CMAKE_C_FLAGS}"
+				CACHE STRING "Common C++ flags for all build configurations." FORCE
+			)
+			SET(
+				CMAKE_EXE_LINKER_FLAGS "" CACHE STRING
+				"Common link flags for all build configurations." FORCE
+			)
 		
-		# Release
-		SET(
-			CMAKE_C_FLAGS_RELEASE
-			"-DNDEBUG -O2 ${visibility_cflags_} ${gc_sections_cflags_}"
-			CACHE STRING "C flags for Release builds." FORCE
-		)
-		SET(
-			CMAKE_CXX_FLAGS_RELEASE
-			"-DNDEBUG -O2 ${visibility_cflags_} ${gc_sections_cflags_}"
-			CACHE STRING "C++ flags for Release builds." FORCE
-		)
-		SET(
-			CMAKE_EXE_LINKER_FLAGS_RELEASE
-			"${gc_sections_ldflags_} ${dead_strip_ldflags_}"
-			CACHE STRING "Link flags for Release builds." FORCE
-		)
+			# Release
+			SET(
+				CMAKE_C_FLAGS_RELEASE
+				"-DNDEBUG -O2 ${visibility_cflags_} ${gc_sections_cflags_}"
+				CACHE STRING "C flags for Release builds." FORCE
+			)
+			SET(
+				CMAKE_CXX_FLAGS_RELEASE
+				"-DNDEBUG -O2 ${visibility_cflags_} ${gc_sections_cflags_}"
+				CACHE STRING "C++ flags for Release builds." FORCE
+			)
+			SET(
+				CMAKE_EXE_LINKER_FLAGS_RELEASE
+				"${gc_sections_ldflags_} ${dead_strip_ldflags_}"
+				CACHE STRING "Link flags for Release builds." FORCE
+			)
+			
+			# MinSizeRel
+			SET(
+				CMAKE_C_FLAGS_MINSIZEREL
+				"-DNDEBUG -Os ${visibility_cflags_} ${gc_sections_cflags_}"
+				CACHE STRING "C flags for MinSizeRel builds." FORCE
+			)
+			SET(
+				CMAKE_CXX_FLAGS_MINSIZEREL
+				"-DNDEBUG -Os ${visibility_cflags_} ${gc_sections_cflags_}"
+				CACHE STRING "C++ flags for MinSizeRel builds." FORCE
+			)
+			SET(
+				CMAKE_EXE_LINKER_FLAGS_MINSIZEREL
+				"${gc_sections_ldflags_} ${dead_strip_ldflags_}"
+				CACHE STRING "Link flags for MinSizeRel builds." FORCE
+			)
+			
+			# RelWithDebInfo
+			SET(
+				CMAKE_C_FLAGS_RELWITHDEBINFO
+				"-DNDEBUG -g -O2 ${visibility_cflags_}"
+				CACHE STRING "C flags for RelWithDebInfo builds." FORCE
+			)
+			SET(
+				CMAKE_CXX_FLAGS_RELWITHDEBINFO
+				"-DNDEBUG -g -O2 ${visibility_cflags_}"
+				CACHE STRING "C++ flags for RelWithDebInfo builds." FORCE
+			)
+			SET(
+				CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO ""
+				CACHE STRING "Link flags for RelWithDebInfo builds." FORCE
+			)
+			
+			# Debug
+			SET(
+				CMAKE_C_FLAGS_DEBUG "-DDEBUG -g" CACHE STRING
+				"C flags for Debug builds." FORCE
+			)
+			SET(
+				CMAKE_CXX_FLAGS_DEBUG "-DDEBUG -g" CACHE STRING
+				"C++ flags for Debug builds." FORCE
+			)
+			SET(
+				CMAKE_EXE_LINKER_FLAGS_DEBUG ""
+				CACHE STRING "Link flags for Debug builds." FORCE
+			)
+			
+			SET(COMPILER_FLAGS_OVERRIDDEN YES CACHE INTERNAL "" FORCE)
 		
-		# MinSizeRel
-		SET(
-			CMAKE_C_FLAGS_MINSIZEREL
-			"-DNDEBUG -Os ${visibility_cflags_} ${gc_sections_cflags_}"
-			CACHE STRING "C flags for MinSizeRel builds." FORCE
-		)
-		SET(
-			CMAKE_CXX_FLAGS_MINSIZEREL
-			"-DNDEBUG -Os ${visibility_cflags_} ${gc_sections_cflags_}"
-			CACHE STRING "C++ flags for MinSizeRel builds." FORCE
-		)
-		SET(
-			CMAKE_EXE_LINKER_FLAGS_MINSIZEREL
-			"${gc_sections_ldflags_} ${dead_strip_ldflags_}"
-			CACHE STRING "Link flags for MinSizeRel builds." FORCE
-		)
+		ENDIF(NOT COMPILER_FLAGS_OVERRIDDEN)
 		
-		# RelWithDebInfo
-		SET(
-			CMAKE_C_FLAGS_RELWITHDEBINFO
-			"-DNDEBUG -g -O2 ${visibility_cflags_}"
-			CACHE STRING "C flags for RelWithDebInfo builds." FORCE
-		)
-		SET(
-			CMAKE_CXX_FLAGS_RELWITHDEBINFO
-			"-DNDEBUG -g -O2 ${visibility_cflags_}"
-			CACHE STRING "C++ flags for RelWithDebInfo builds." FORCE
-		)
-		SET(
-			CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO ""
-			CACHE STRING "Link flags for RelWithDebInfo builds." FORCE
-		)
-		
-		# Debug
-		SET(
-			CMAKE_C_FLAGS_DEBUG "-DDEBUG -g" CACHE STRING
-			"C flags for Debug builds." FORCE
-		)
-		SET(
-			CMAKE_CXX_FLAGS_DEBUG "-DDEBUG -g" CACHE STRING
-			"C++ flags for Debug builds." FORCE
-		)
-		SET(
-			CMAKE_EXE_LINKER_FLAGS_DEBUG ""
-			CACHE STRING "Link flags for Debug builds." FORCE
-		)
 	ENDIF(CMAKE_COMPILER_IS_GNUCC AND CMAKE_COMPILER_IS_GNUCXX
 	      AND CMAKE_C_FLAGS MATCHES "\\s*" AND CMAKE_CXX_FLAGS MATCHES "\\s*")
 ENDMACRO(ST_SET_DEFAULT_GCC_FLAGS)
