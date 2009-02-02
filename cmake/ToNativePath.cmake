41220b7c8bd1305aa7c019ebe638836a0f608cd5
jart <jart@80b70061-164d-0410-b7b7-e274ec932871>
2009-02-02 13:57:55 +0000

    NSIS doesn't like forward slashes as directory separators.
    git-svn-id: https://scantailor.svn.sourceforge.net/svnroot/scantailor/trunk@254 80b70061-164d-0410-b7b7-e274ec932871

---
+++ cmake/ToNativePath.cmake
@@ -0,0 +1,7 @@
+# This macro exists because FILE(TO_NATIVE_PATH ...) is broken on MinGW.
+MACRO(TO_NATIVE_PATH PATH OUT)
+	FILE(TO_NATIVE_PATH "${PATH}" "${OUT}")
+	IF(MINGW)
+		STRING(REPLACE "/" "\\" "${OUT}" "${${OUT}}")
+	ENDIF(MINGW)
+ENDMACRO(TO_NATIVE_PATH)
