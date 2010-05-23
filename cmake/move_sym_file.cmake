9006269871e12ecacdd6a082249383ca6504e30e
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2010-05-23 18:43:37 +0100

    Dump symbols when building installer, not when building scantailor executable.

--- cmake/move_sym_file.cmake
+++ cmake/move_sym_file.cmake
@@ -1,4 +1,4 @@
-FILE(STRINGS "temp.sym" first_line LIMIT_COUNT 1)
+FILE(STRINGS "${SYMBOLS_PATH}/temp.sym" first_line LIMIT_COUNT 1)
 SEPARATE_ARGUMENTS(first_line)
 LIST(GET first_line 3 module_id)
 LIST(GET first_line 4 module_name_pdb)
@@ -6,5 +6,9 @@ STRING(
 	REGEX REPLACE "(.*)\\.pdb" "\\1.sym"
 	module_name_sym "${module_name_pdb}"
 )
-CONFIGURE_FILE("temp.sym" "./${module_name_pdb}/${module_id}/${module_name_sym}" COPYONLY)
-FILE(REMOVE "temp.sym")
+CONFIGURE_FILE(
+	"${SYMBOLS_PATH}/temp.sym"
+	"${SYMBOLS_PATH}/${module_name_pdb}/${module_id}/${module_name_sym}"
+	COPYONLY
+)
+FILE(REMOVE "${SYMBOLS_PATH}/temp.sym")
