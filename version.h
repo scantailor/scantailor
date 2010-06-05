52f3ed2a822fc0191f2e96531a9ed3d04c41890f
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2010-06-05 22:09:48 +0100

    Set version to 0.9.9rc1

--- version.h
+++ version.h
@@ -1,7 +1,25 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C)  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef SCANTAILOR_VERSION_H_
 #define SCANTAILOR_VERSION_H_
 
-#define VERSION "0.9.9pre4"
-#define VERSION_QUAD ""
+#define VERSION "0.9.9rc1"
+#define VERSION_QUAD "" // Must be "x.x.x.x" or an empty string.
 
 #endif
