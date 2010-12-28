06c7241a9a2c869b3334471a4742edd9fa1b3cc7
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2010-12-28 15:20:57 +0000

    Set version to 1.0.0beta1

--- version.h
+++ version.h
@@ -19,7 +19,7 @@
 #ifndef SCANTAILOR_VERSION_H_
 #define SCANTAILOR_VERSION_H_
 
-#define VERSION "0.9.10pre"
+#define VERSION "1.0.0beta1"
 #define VERSION_QUAD "" // Must be "x.x.x.x" or an empty string.
 
 #endif
