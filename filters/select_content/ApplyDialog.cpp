1d89435f3e791bd29d4d44df32561eb71bd57cde
Masahiro Kitagawa <kitashi@users.sourceforge.net>
2011-01-17 20:01:36 +0900

    commit missng files.

---
+++ filters/select_content/ApplyDialog.cpp
@@ -0,0 +1,94 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C)  Joseph Artsimovich <joseph.artsimovich@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ApplyDialog.h"
+#include "ApplyDialog.h.moc"
+#include "PageSelectionAccessor.h"
+#include <QButtonGroup>
+#include <QDebug>
+#include <assert.h>
+
+namespace select_content
+{
+
+ApplyDialog::ApplyDialog(
+	QWidget* parent,
+	PageId const& cur_page, PageSelectionAccessor const& page_selection_accessor)
+:	QDialog(parent),
+	m_pages(page_selection_accessor.allPages()),
+	m_selectedPages(page_selection_accessor.selectedPages()),
+	m_selectedRanges(page_selection_accessor.selectedRanges()),
+	m_curPage(cur_page),
+	m_pBtnGroup(new QButtonGroup(this))
+{
+	setupUi(this);
+	m_pBtnGroup->addButton(thisPageOnlyRB);
+	m_pBtnGroup->addButton(allPagesRB);
+	m_pBtnGroup->addButton(thisPageAndFollowersRB);
+	m_pBtnGroup->addButton(selectedPagesRB);
+	m_pBtnGroup->addButton(everyOtherRB);
+	m_pBtnGroup->addButton(everyOtherSelectedRB);
+	
+	if (m_selectedPages.size() <= 1) {
+		selectedPagesWidget->setEnabled(false);
+		everyOtherSelectedWidget->setEnabled(false);
+		everyOtherSelectedHint->setText(selectedPagesHint->text());
+	} else if (m_selectedRanges.size() > 1) {
+		everyOtherSelectedWidget->setEnabled(false);
+		everyOtherSelectedHint->setText(tr("Can't do: more that one group is selected."));
+	}
+	
+	connect(buttonBox, SIGNAL(accepted()), this, SLOT(onSubmit()));
+}
+
+ApplyDialog::~ApplyDialog()
+{
+}
+
+void
+ApplyDialog::onSubmit()
+{
+	std::set<PageId> pages;
+	
+	// thisPageOnlyRB is intentionally not handled.
+	if (allPagesRB->isChecked()) {
+		m_pages.selectAll().swap(pages);
+		emit applySelection(pages);
+		accept();
+		return;
+	} else if (thisPageAndFollowersRB->isChecked()) {
+		m_pages.selectPagePlusFollowers(m_curPage).swap(pages);
+	} else if (selectedPagesRB->isChecked()) {
+		emit applySelection(m_selectedPages);
+		accept();
+		return;
+	} else if (everyOtherRB->isChecked()) {
+		m_pages.selectEveryOther(m_curPage).swap(pages);
+	} else if (everyOtherSelectedRB->isChecked()) {
+		assert(m_selectedRanges.size() == 1);
+		PageRange const& range = m_selectedRanges.front();
+		range.selectEveryOther(m_curPage).swap(pages);
+	}
+	
+	emit applySelection(pages);
+	
+	// We assume the default connection from accept() to accepted() was removed.
+	accept();
+}
+
+} // namespace select_content
---
+++ filters/select_content/ApplyDialog.h
@@ -0,0 +1,61 @@
+/*
+    Scan Tailor - Interactive post-processing tool for scanned pages.
+    Copyright (C) 2007-2008  Joseph Artsimovich <joseph_a@mail.ru>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SELECT_CONTENT_APPLYDIALOG_H_
+#define SELECT_CONTENT_APPLYDIALOG_H_
+
+#include "ui_SelectContentApplyDialog.h"
+#include "PageId.h"
+#include "PageRange.h"
+#include "PageSequence.h"
+#include "IntrusivePtr.h"
+#include <QDialog>
+#include <vector>
+#include <set>
+
+class PageSelectionAccessor;
+class QButtonGroup;
+
+namespace select_content
+{
+
+class Scope;
+
+class ApplyDialog : public QDialog, private Ui::SelectContentApplyDialog
+{
+	Q_OBJECT
+public:
+	ApplyDialog(QWidget* parent, PageId const& cur_page,
+		PageSelectionAccessor const& page_selection_accessor);
+	
+	virtual ~ApplyDialog();
+signals:
+	void applySelection(std::set<PageId> const& pages);
+private slots:
+	void onSubmit();
+private:
+	PageSequence m_pages;
+	std::set<PageId> m_selectedPages;
+	std::vector<PageRange> m_selectedRanges;
+	PageId m_curPage;
+	QButtonGroup* m_pBtnGroup;
+};
+
+} // namespace select_content
+
+#endif
