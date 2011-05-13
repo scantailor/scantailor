787cc2fe7ded575cdb2bd76135b60220b2d514f8
Petr Kovar <pejuko@gmail.com>
2011-05-13 11:09:00 +0200

    save pictureShape

--- filters/output/Params.cpp
+++ filters/output/Params.cpp
@@ -42,7 +42,7 @@ Params::Params(QDomElement const& el)
 	m_depthPerception(el.attribute("depthPerception")),
 	m_dewarpingMode(el.attribute("dewarpingMode")),
 	m_despeckleLevel(despeckleLevelFromString(el.attribute("despeckleLevel"))),
-	m_pictureShape(FREE_SHAPE)
+	m_pictureShape((PictureShape)(el.attribute("pictureShape").toInt()))
 {
 	QDomElement const cp(el.namedItem("color-params").toElement());
 	m_colorParams.setColorMode(parseColorMode(cp.attribute("colorMode")));
@@ -63,6 +63,7 @@ Params::toXml(QDomDocument& doc, QString const& name) const
 	
 	QDomElement el(doc.createElement(name));
 	el.appendChild(m_distortionModel.toXml(doc, "distortion-model"));
+	el.setAttribute("pictureShape", (int)m_pictureShape);
 	el.setAttribute("depthPerception", m_depthPerception.toString());
 	el.setAttribute("dewarpingMode", m_dewarpingMode.toString());
 	el.setAttribute("despeckleLevel", despeckleLevelToString(m_despeckleLevel));
