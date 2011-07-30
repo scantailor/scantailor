5ad5d4e23c60ff3df2e54f82f7eb5f091739e36c
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2011-07-30 20:58:39 +0100

    Set version to 0.9.10

--- version.h
+++ version.h
@@ -19,7 +19,7 @@
 #ifndef SCANTAILOR_VERSION_H_
 #define SCANTAILOR_VERSION_H_
 
-#define VERSION "0.9.10rc1"
-#define VERSION_QUAD "" // Must be "x.x.x.x" or an empty string.
+#define VERSION "0.9.10"
+#define VERSION_QUAD "0.9.10.0" // Must be "x.x.x.x" or an empty string.
 
 #endif
