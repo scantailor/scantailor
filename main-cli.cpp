a02cd61c7e86c51f009dae92e3c9a87ead2204af
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2012-01-02 19:29:29 +0000

    Fix loading Qt plugins on systems where we don't distribute them ourselves.

--- main-cli.cpp
+++ main-cli.cpp
@@ -30,8 +30,10 @@ int main(int argc, char **argv)
 {
 	QCoreApplication app(argc, argv);
 
+#ifdef _WIN32
 	// Get rid of all references to Qt's installation directory.
 	app.setLibraryPaths(QStringList(app.applicationDirPath()));
+#endif
 
 	// parse command line arguments
 	CommandLine cli(app.arguments(), false);
