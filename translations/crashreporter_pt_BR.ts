db082ca225c5a74234d049bbaecd4a7eb5bbbed9
Joseph Artsimovich <joseph.artsimovich@gmail.com>
2012-02-25 19:35:58 +0000

    New translation: Brazilian Portuguese.

---
+++ translations/crashreporter_pt_BR.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR">
+<context>
+    <name>CrashReportDialog</name>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="71"/>
+        <source>This file contains the internal state of the program when it crashed</source>
+        <translation>Este arquivo contem o estado interno do programa quando este trava</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="77"/>
+        <source>Dump file</source>
+        <translation>Arquivo de dump</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="105"/>
+        <source>Sending ...</source>
+        <translation>Enviando ...</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="136"/>
+        <source>Unexpected response (code %1) from dispatcher</source>
+        <translation>Resposta inesperada (codigo %1) ao enviar</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="161"/>
+        <source>Unexpected response from dispatcher</source>
+        <translation>Resposta inesperada ao enviar</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="221"/>
+        <source>Unexpected response (code %1) from the receiving side</source>
+        <translation>Resposta inesperada (codigo %1) do receptor</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/CrashReportDialog.cpp" line="227"/>
+        <source>Successfully sent</source>
+        <translation>Enviado com sucesso</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/ui/CrashReportDialog.ui" line="14"/>
+        <source>Crash Report</source>
+        <translation>Relatório de travamento</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/ui/CrashReportDialog.ui" line="20"/>
+        <source>Scan Tailor crashed.  That&apos;s usually caused by errors in the software.  You can help us find and fix the problem by submitting this crash report.</source>
+        <translation>Scan Tailor travou. Isso seguramente é algum erro no programa.Você pode ajudar a resolver o problema enviando este relatório.</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/ui/CrashReportDialog.ui" line="30"/>
+        <source>Information to be sent</source>
+        <translation>Informação a ser enviada</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/ui/CrashReportDialog.ui" line="63"/>
+        <source>If you have additional information, for example you know how to reproduce the crash, please provide it here.
+English or Russian please.</source>
+        <translation>Se você tem alguma informação adicional, por exemplo sabe como reproducir o travamento, por favor documente aqui em Inglês ou Russo.</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/ui/CrashReportDialog.ui" line="71"/>
+        <source>If you are willing to provide further assistance to developers to help fix this problem, please provide your email address.</source>
+        <translation>Se você esta disposto a dar assistencia aos programadores para solucionar o erro, por favor informe seu email.</translation>
+    </message>
+    <message>
+        <location filename="../crash_reporter/ui/CrashReportDialog.ui" line="81"/>
+        <source>Email [optional]</source>
+        <translation>Email [opcional]</translation>
+    </message>
+</context>
+</TS>
