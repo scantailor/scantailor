d2f2a0d519c0c86716d264f6b4d3e0f82c59e3c4
Andrew Ross <ubuntu@rossfamily.co.uk>
2015-02-18 19:39:15 +0000

    Scantailor Featured - Ortho_Corner_Move_Square_Picture_Zones
    Changes by monday2000

--- zones/ZoneVertexDragInteraction.cpp
+++ zones/ZoneVertexDragInteraction.cpp
@@ -128,6 +128,40 @@ ZoneVertexDragInteraction::onMouseMoveEvent(QMouseEvent* event, InteractionState
 {
 	QTransform const from_screen(m_rContext.imageView().widgetToImage());
 	m_ptrVertex->setPoint(from_screen.map(event->pos() + QPointF(0.5, 0.5) + m_dragOffset));
+
+//begin of modified by monday2000
+//Ortho_Corner_Move_Square_Picture_Zones
+//added:
+
+	Qt::KeyboardModifiers mask = event->modifiers();
+
+	if (mask == Qt::ControlModifier) 
+	{
+		QTransform const to_screen(m_rContext.imageView().imageToWidget());
+
+		QPointF current = event->pos() + QPointF(0.5, 0.5) + m_dragOffset;
+		QPointF prev = to_screen.map(m_ptrVertex->prev(SplineVertex::LOOP)->point());
+		QPointF next = to_screen.map(m_ptrVertex->next(SplineVertex::LOOP)->point());
+
+		int dx = next.x() - prev.x();
+		int dy = next.y() - prev.y();
+
+		if ((dx > 0 && dy > 0) || (dx < 0 && dy < 0))
+		{
+			prev.setX(current.x());
+			next.setY(current.y());
+		}
+		else
+		{
+			next.setX(current.x());	
+			prev.setY(current.y());
+		}		
+
+		m_ptrVertex->prev(SplineVertex::LOOP)->setPoint(from_screen.map(prev));
+		m_ptrVertex->next(SplineVertex::LOOP)->setPoint(from_screen.map(next));
+	}
+//end of modified by monday2000
+
 	checkProximity(interaction);
 	m_rContext.imageView().update();
 }
