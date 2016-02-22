01bb6f9eac92882c270b532c03416d965dd711ac
princeofdarkness76 <djsteviejames@hotmail.com>
2016-02-22 05:49:39 -0800

    pod/scantailor-makepp.mk
    added makepp.mk files

--- .makepp/scantailor_ja.qm.mk
+++ .makepp/scantailor_ja.qm.mk
@@ -1,13 +1,13 @@
-COMMAND=@/usr/local/Cellar/cmake/3.4.3/bin/cmake -E cmake_echo_color --switch= --blue --bold --progress-dir=/Users/pod/scantailor/CMakeFiles --progress-num= "Generating scantailor_ja.qm"/usr/local/Cellar/qt/4.8.7_2/bin/lrelease /Users/pod/scantailor/translations/scantailor_ja.ts -qm /Users/pod/scantailor/scantailor_ja.qm
-INCLUDE_PATHS=
-IMPLICIT_DEPS=
-IMPLICIT_TARGETS=
-BUILD_SIGNATURE=1456116805,38660
-META_DEPS=/usr/local/Cellar/cmake/3.4.3/bin/cmake/usr/local/Cellar/qt/4.8.7_2/bin/lrelease
-SORTED_DEPS=/usr/local/cellar/cmake/3.4.3/bin/cmake/usr/local/cellar/qt/4.8.7_2/bin/lreleasetranslations/scantailor_ja.ts
 ARCH=darwin-thread-multi-2level
-INCLUDE_SFXS=
+COMMAND=@/usr/local/Cellar/cmake/3.4.3/bin/cmake -E cmake_echo_color --switch= --blue --bold --progress-dir=/Users/pod/scantailor/CMakeFiles --progress-num= "Generating scantailor_ja.qm"/usr/local/Cellar/qt/4.8.7_2/bin/lrelease /Users/pod/scantailor/translations/scantailor_ja.ts -qm /Users/pod/scantailor/scantailor_ja.qm
+META_DEPS=/usr/local/Cellar/cmake/3.4.3/bin/cmake/usr/local/Cellar/qt/4.8.7_2/bin/lrelease
+BUILD_SIGNATURE=1456127474,38660
+INCLUDE_PATHS=
 CWD=.
-SIGNATURE=1456116805,38660
+IMPLICIT_DEPS=
 DEP_SIGS=1453741043,51793081431008909,19610681456113606,118466
+INCLUDE_SFXS=
+SORTED_DEPS=/usr/local/cellar/cmake/3.4.3/bin/cmake/usr/local/cellar/qt/4.8.7_2/bin/lreleasetranslations/scantailor_ja.ts
+SIGNATURE=1456127474,38660
+IMPLICIT_TARGETS=
 END=
--- .makepp/scantailor_ru.qm.mk
+++ .makepp/scantailor_ru.qm.mk
@@ -1,13 +1,13 @@
-META_DEPS=/usr/local/Cellar/cmake/3.4.3/bin/cmake/usr/local/Cellar/qt/4.8.7_2/bin/lrelease
-BUILD_SIGNATURE=1456116805,53875
-IMPLICIT_TARGETS=
-IMPLICIT_DEPS=
+ARCH=darwin-thread-multi-2level
 COMMAND=@/usr/local/Cellar/cmake/3.4.3/bin/cmake -E cmake_echo_color --switch= --blue --bold --progress-dir=/Users/pod/scantailor/CMakeFiles --progress-num=3 "Generating scantailor_ru.qm"/usr/local/Cellar/qt/4.8.7_2/bin/lrelease /Users/pod/scantailor/translations/scantailor_ru.ts -qm /Users/pod/scantailor/scantailor_ru.qm
+BUILD_SIGNATURE=1456127474,53875
+META_DEPS=/usr/local/Cellar/cmake/3.4.3/bin/cmake/usr/local/Cellar/qt/4.8.7_2/bin/lrelease
 INCLUDE_PATHS=
 CWD=.
-SIGNATURE=1456116805,53875
-DEP_SIGS=1453741043,51793081431008909,19610681456113606,135592
+IMPLICIT_DEPS=
 INCLUDE_SFXS=
-ARCH=darwin-thread-multi-2level
+DEP_SIGS=1453741043,51793081431008909,19610681456113606,135592
 SORTED_DEPS=/usr/local/cellar/cmake/3.4.3/bin/cmake/usr/local/cellar/qt/4.8.7_2/bin/lreleasetranslations/scantailor_ru.ts
+SIGNATURE=1456127474,53875
+IMPLICIT_TARGETS=
 END=
