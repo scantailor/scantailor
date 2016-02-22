94908c06bfe4c9cfc6869c50a0df6f631ba32383
princeofdarkness76 <djsteviejames@hotmail.com>
2016-02-21 23:55:04 -0800

    pod/makepp
    added makepp files

--- .makepp/scantailor_bg.qm.mk
+++ .makepp/scantailor_bg.qm.mk
@@ -1,13 +1,13 @@
-INCLUDE_PATHS=
-COMMAND=@/usr/local/Cellar/cmake/3.4.3/bin/cmake -E cmake_echo_color --switch= --blue --bold --progress-dir=/Users/pod/scantailor/CMakeFiles --progress-num= "Generating scantailor_bg.qm"/usr/local/Cellar/qt/4.8.7_2/bin/lrelease /Users/pod/scantailor/translations/scantailor_bg.ts -qm /Users/pod/scantailor/scantailor_bg.qm
-IMPLICIT_TARGETS=
-IMPLICIT_DEPS=
-BUILD_SIGNATURE=1456116804,50537
-META_DEPS=/usr/local/Cellar/cmake/3.4.3/bin/cmake/usr/local/Cellar/qt/4.8.7_2/bin/lrelease
 SORTED_DEPS=/usr/local/cellar/cmake/3.4.3/bin/cmake/usr/local/cellar/qt/4.8.7_2/bin/lreleasetranslations/scantailor_bg.ts
-ARCH=darwin-thread-multi-2level
-INCLUDE_SFXS=
-CWD=.
-SIGNATURE=1456116804,50537
+SIGNATURE=1456127473,50537
+IMPLICIT_TARGETS=
 DEP_SIGS=1453741043,51793081431008909,19610681456113606,125570
+INCLUDE_SFXS=
+IMPLICIT_DEPS=
+CWD=.
+INCLUDE_PATHS=
+BUILD_SIGNATURE=1456127473,50537
+ARCH=darwin-thread-multi-2level
+COMMAND=@/usr/local/Cellar/cmake/3.4.3/bin/cmake -E cmake_echo_color --switch= --blue --bold --progress-dir=/Users/pod/scantailor/CMakeFiles --progress-num= "Generating scantailor_bg.qm"/usr/local/Cellar/qt/4.8.7_2/bin/lrelease /Users/pod/scantailor/translations/scantailor_bg.ts -qm /Users/pod/scantailor/scantailor_bg.qm
+META_DEPS=/usr/local/Cellar/cmake/3.4.3/bin/cmake/usr/local/Cellar/qt/4.8.7_2/bin/lrelease
 END=
